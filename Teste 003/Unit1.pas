577a10eb7243cd22522700fe8be2b177adf3d0e3
alex.suttili@gmail.com <suttili>
2020-07-05 16:57:45 -0300

    Teste github 003

---
+++ Teste 003/Unit1.pas
@@ -0,0 +1,39 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Button1: TButton;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Teste 3 GitHub');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+   ShowMessage(Edit1.text);
+end;
+
+end.
